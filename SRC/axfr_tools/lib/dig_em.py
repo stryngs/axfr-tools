e55bacf4b844c2bba85ecacba52251d563936c8b
stryngs <info@ethicalreporting.org>
2018-01-03 00:04:30 -0500

    Added threading counter

--- SRC/axfr_tools/lib/dig_em.py
+++ SRC/axfr_tools/lib/dig_em.py
@@ -3,9 +3,14 @@ import time
 
 class Dig(object):
     """Wrapper class for the dig-em bash script"""
+
+    def __init__(self):
+        self.counter = 0
+
     
     def singleDig(self, domain):
         os.system('dig-em -d %s pythonS' % domain)
+    
     
     def multiDig(self, domain):
         os.system('dig-em -d %s pythonM' % domain)
