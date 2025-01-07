e4b7245afe6314794b5b125553b1769ae3227ac9
stryngs <support@configitnow.com>
2025-01-07 02:23:42 -0500

    fixed -p issues

--- SRC/axfr_tools/lib/db_purge.py
+++ SRC/axfr_tools/lib/db_purge.py
@@ -12,40 +12,19 @@ class Purge(object):
         dbFile = input('Tgt DB?\n')
         with open(pFile, 'r') as iFile:
             pList = list(set(iFile.read().splitlines()))
-        con = lite.connect(dbFile, isolation_level = None)    ### Test for speed
+        con = lite.connect(dbFile, isolation_level = None)
         db = con.cursor()
         count = 1
-
-        ## Iterate through each domain finding nameservers
         with con:
             for dm in pList:
-
-                ## Purge the easy stuff
                 print(f'Purging {count} -- {dm}')
                 db.execute("DELETE FROM axfr WHERE dm = ?;", (dm,))
                 db.execute("DELETE FROM scanned WHERE dm = ?;", (dm,))
                 db.execute("DELETE FROM domains WHERE dm = ?;", (dm,))
-                db.execute("DELETE FROM nameservers WHERE dm = ?;", (dm,))
                 db.execute("DELETE FROM issues WHERE dm = ?;", (dm,))
-
-                ## Obtain a list from dm2ns
-                q = db.execute("SELECT `ns` FROM `dm2ns` WHERE LOWER(`dm`) = ?;", (dm.lower(),))
-                dnList = [i[0] for i in q.fetchall()]
-
-                ## Take dnList and play with counts
-                for dn in dnList:
-                    q = db.execute("SELECT COUNT(`ns`) FROM `dm2ns` WHERE LOWER(`ns`) = ?;", (dn.lower(),))
-                    nCount = int(q.fetchone()[0])
-
-                    ## Remove the entry from dm2ns
-                    db.execute("DELETE FROM `dm2ns` WHERE LOWER(`dm`) = ? and LOWER(`ns`) = ?;", (dm.lower(), dn.lower()))
-
-                    ## If nCount is 1, then it is safe to purge from nameservers
-                    if nCount == 1:
-                        db.execute("DELETE FROM `nameservers` WHERE LOWER(`ns`) = ?;", (dn.lower(),))
+                db.execute("DELETE FROM nameservers WHERE ns NOT IN (SELECT ns from dm2ns WHERE dm = ?);", (dm,))
+                db.execute("DELETE FROM issues WHERE dm = ?;", (dm,))
                 con.commit()
                 count += 1
         con.close()
-
-        ## Declare complete
         print('Finished!\n')
