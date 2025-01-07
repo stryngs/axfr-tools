ad039626f840d50bbf58fa46a4ad851ce84eb0ce
stryngs <support@configitnow.com>
2025-01-07 01:31:12 -0500

    deprecated old ways

--- SRC/axfr_tools/__init__.py
+++ SRC/axfr_tools/__init__.py
@@ -2,8 +2,6 @@
 
 from .lib.axfr import Axfr
 from .lib.comparisons import Compare
-from .lib.dig_em import Dig
-from .lib.db_builder import Builder
 from .lib.db_purge import Purge
 from .lib.merger import Merger
 from .lib.queries import Queries
