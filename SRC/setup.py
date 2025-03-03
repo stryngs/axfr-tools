58f16e3d803c07942850e59cf0c3b26d159296e2
stryngs <support@configitnow.com>
2025-03-03 00:29:17 -0500

    1.2.4 release

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'axfr-tools',
-    version = '1.2.2',
+    version = '1.2.4',
     author = 'stryngs',
     author_email = 'info@127.0.0.1',
     packages = ['axfr_tools', 'axfr_tools.lib'],
